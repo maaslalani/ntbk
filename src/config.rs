df1c150e95f763c9f8992712e4fca660c8e45aa9
Maas Lalani <maaslalani1@gmail.com>
2020-06-27 17:42:07 -0400

    oops

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub const LOCATION: &str = "/Users/maas/config/ntbk";
+pub const LOCATION: &str = "/Users/maas/.config/ntbk";
 pub const EXTENSION: &str = "md";
 pub const EDITOR: &str = "nvim";
 pub const GREP_COMMAND: &str = "rg";
