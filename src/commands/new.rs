5a2419374e13a5873e331990ecaecafe71eaf92a
Maas Lalani <maaslalani1@gmail.com>
2020-06-27 19:40:52 -0400

    use dialoguer

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,8 +1,14 @@
+use dialoguer::Input;
 use crate::config;
 use std::fs::File;
 
 pub fn run() {
-    let name = std::env::args().nth(2).expect("No name provided");
+    let name = if std::env::args().len() < 3 {
+        Input::<String>::new().with_prompt("Name").interact().unwrap()
+    } else {
+        std::env::args().nth(2).unwrap()
+    };
+
     match File::create(format!("{}/{}.{}", config::LOCATION, name, config::EXTENSION)) {
         Ok(_) => println!("Note {} created.", name),
         Err(_) => println!("Failed to create note."),
