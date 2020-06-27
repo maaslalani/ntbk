76bf1d3479976e7f792d28e3dfb88abcafa5ab81
Maas Lalani <maaslalani1@gmail.com>
2020-06-27 13:25:49 -0400

    implement scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let action = std::env::args().nth(1).expect("No action provided");
+    let name = std::env::args().nth(2).expect("No name provided");
+
+    match action.as_str() {
+        "new" => println!("TODO"),
+        "list" => println!("TODO"),
+        "find" => println!("TODO"),
+        "grep" => println!("TODO"),
+        "open" => println!("TODO"),
+        "show" => println!("TODO"),
+        "remove" => println!("TODO"),
+        "help" => println!("TODO"),
+        _ => println!("Unexpected action"),
+    }
 }
