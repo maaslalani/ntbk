d07414fe61ef937444443ac4ff413232c1e80b5d
Maas Lalani <maaslalani1@gmail.com>
2020-06-27 23:04:05 -0400

    add help

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,3 +1,17 @@
 pub fn run() {
-    println!("TODO");
+    println!("ntbk is a terminal notebook manager.\n");
+
+    println!("Usage: ntbk [ACTION]");
+    println!("  ntbk find   <pattern>  - Find notes by path");
+    println!("  ntbk grep   <pattern>  - Search through notes content");
+    println!("  ntbk help              - Print this usage information");
+    println!("  ntbk list              - List all notes");
+    println!("  ntbk new    <name>     - Create a new note with <name>");
+    println!("  ntbk open   <name>     - Open a note");
+    println!("  ntbk remove <name>     - Remove a note");
+    println!("  ntbk show   <name>     - Show a note's content");
+
+    println!("\nAll <name> arguments are optional");
+    println!("if none is provided then you will be presented with a list of your notes to choose from");
+    println!("or an input box where appropriate");
 }
