862b69eec74883118af96b8d3619095751b39aab
Maas Lalani <maaslalani1@gmail.com>
2020-06-28 11:46:51 -0400

    use multiline string for help

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,16 +1,21 @@
+const HELP: &str = r#"
+ntbk is a terminal notebook manager.
+
+Usage: ntbk [ACTION]
+
+    ntbk find   <pattern>  - Find notes by path
+    ntbk grep   <pattern>  - Search through notes content
+    ntbk help              - Print this usage information
+    ntbk list              - List all notes
+    ntbk new    <name>     - Create a new note with <name>
+    ntbk open   <name>     - Open a note
+    ntbk remove <name>     - Remove a note
+    ntbk show   <name>     - Show a note's content
+
+Arguments <name> and <pattern> are optional.
+if none are provided, ntbk will prompt for a choice / input.
+"#;
+
 pub fn run() {
-    println!("ntbk is a terminal notebook manager.\n");
-
-    println!("Usage: ntbk [ACTION]");
-    println!("  ntbk find   <pattern>  - Find notes by path");
-    println!("  ntbk grep   <pattern>  - Search through notes content");
-    println!("  ntbk help              - Print this usage information");
-    println!("  ntbk list              - List all notes");
-    println!("  ntbk new    <name>     - Create a new note with <name>");
-    println!("  ntbk open   <name>     - Open a note");
-    println!("  ntbk remove <name>     - Remove a note");
-    println!("  ntbk show   <name>     - Show a note's content");
-
-    println!("\nAll <name> arguments are optional");
-    println!("if none is provided then you will be presented with a list or an input box");
+    println!("{}", HELP);
 }
