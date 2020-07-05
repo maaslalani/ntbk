cfa596841aa4d6322fe55018ba29b3011fcd42c1
Maas Lalani <maaslalani1@gmail.com>
2020-07-05 00:19:19 -0400

    add / if necessary

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,10 @@ use dirs;
 
 pub fn directory() -> String {
     match std::env::var("NOTES_DIRECTORY") {
-        Ok(val) => val,
+        Ok(val) => {
+            let suffix = if val.ends_with("/") { "" } else { "/" };
+            val + suffix
+        },
         Err(_) => {
             let home_dir = match dirs::home_dir() {
                 Some(path) => path.display().to_string(),
